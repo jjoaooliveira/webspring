1eb5a905d793462d949473395375f53267ad0835
Joao Oliveira <88111035+jjoaooliveira@users.noreply.github.com>
2025-01-06 19:34:11 -0400

    refactoring save task test

--- usecase/src/test/java/TaskUseCase/SaveTaskUseCaseTest.java
+++ usecase/src/test/java/TaskUseCase/SaveTaskUseCaseTest.java
@@ -1,10 +1,10 @@
 package TaskUseCase;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
-import com.webapp.entity.Content;
+
 import com.webapp.entity.Task;
-import com.webapp.entity.Title;
 import com.webapp.entity.exceptions.EmptyTextException;
 import com.webapp.entity.exceptions.TextLengthOverLimitException;
 import com.webapp.usecase.dataaccess.TaskRepository;
@@ -20,19 +20,25 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
-
 public class SaveTaskUseCaseTest {
     @Mock
-    TaskRepository taskRepository;
-
+    TaskRepository mockTaskRepository;
     @Mock
-    TaskMapper taskMapper;
-
+    TaskMapper mockTaskMapper;
     @Mock
-    DatabaseMapper databaseMapper;
-
+    DatabaseMapper mockDatabaseMapper;
+    @Mock
+    InputTaskDTO mockTaskDTO;
+    @Mock
+    Task mockTask;
+    @Mock
+    DatabaseTaskDTO mockInputDatabaseTaskDTO;
+    @Mock
+    DatabaseTaskDTO mockOutputDatabaseTaskDTO;
+    @Mock
+    Task mockResponseTask;
+    @Mock
+    OutputTaskDTO mockOutputTaskDTO;
     @InjectMocks
     SaveTaskUseCase saveTaskUseCase;
 
@@ -42,75 +48,15 @@ public class SaveTaskUseCaseTest {
     }
 
     @Test
-    void shouldReturnSavedTaskDataFromDataBase() throws TextLengthOverLimitException, EmptyTextException {
-        LocalDateTime expirationLocalDateTime = LocalDateTime.now();
-        
-        InputTaskDTO taskDTO = new InputTaskDTO(
-                Optional.empty(),
-                Optional.of("Teste"),
-                Optional.of("Conteudo 1"),
-                Optional.of(expirationLocalDateTime.toString())
-        );
+    void givenInputObject_whenExecute_thenReturnSavedObject() throws TextLengthOverLimitException, EmptyTextException {
 
-        Task task = new Task(
-                new Title(taskDTO.title().get()),
-                Content.create(taskDTO.content().get()),
-                LocalDateTime.parse(taskDTO.expirationDate().get())
-        );
+        when(mockTaskMapper.createEntity(mockTaskDTO)).thenReturn(mockTask);
+        when(mockDatabaseMapper.createDTO(mockTask)).thenReturn(mockInputDatabaseTaskDTO);
+        when(mockTaskRepository.save(mockInputDatabaseTaskDTO)).thenReturn(mockOutputDatabaseTaskDTO);
+        when(mockDatabaseMapper.createEntity(mockOutputDatabaseTaskDTO)).thenReturn(mockResponseTask);
+        when(mockTaskMapper.createDTO(mockResponseTask)).thenReturn(mockOutputTaskDTO);
 
-        DatabaseTaskDTO inputDatabaseTaskDTO = new DatabaseTaskDTO(
-                task.getId(),
-                task.getTitle(),
-                task.getContent(),
-                task.getCreationDate().toString(),
-                task.getExpirationDate().toString(),
-                task.isCompleted()
-        );
-
-        DatabaseTaskDTO outputDatabaseTaskDTO = new DatabaseTaskDTO(
-                1L,
-                task.getTitle(),
-                task.getContent(),
-                task.getCreationDate().toString(),
-                task.getExpirationDate().toString(),
-                task.isCompleted()
-        );
-
-        Task responseTask = new Task(
-                outputDatabaseTaskDTO.id(),
-                new Title(outputDatabaseTaskDTO.title()),
-                Content.create(outputDatabaseTaskDTO.content()),
-                outputDatabaseTaskDTO.creationDate(),
-                outputDatabaseTaskDTO.expirationDate(),
-                outputDatabaseTaskDTO.completed()
-        );
-
-        OutputTaskDTO outputTaskDTO = new OutputTaskDTO(
-                responseTask.getId(),
-                responseTask.getTitle(),
-                responseTask.getContent(),
-                responseTask.getCreationDate().toLocalDate().toString(),
-                responseTask.getCreationDate().toLocalTime().toString(),
-                responseTask.getExpirationDate().toLocalDate().toString(),
-                responseTask.getExpirationDate().toLocalTime().toString(),
-                responseTask.getTimeLeft(),
-                responseTask.isCompleted(),
-                responseTask.isExpired()
-        );
-
-        when(taskMapper.toEntity(taskDTO)).thenReturn(task);
-        when(databaseMapper.toDTO(task)).thenReturn(inputDatabaseTaskDTO);
-        when(taskRepository.save(inputDatabaseTaskDTO)).thenReturn(outputDatabaseTaskDTO);
-        when(databaseMapper.toEntity(outputDatabaseTaskDTO)).thenReturn(responseTask);
-        when(taskMapper.toDTO(responseTask)).thenReturn(outputTaskDTO);
-
-        OutputTaskDTO result = saveTaskUseCase.execute(taskDTO);
-
-        assertEquals(task.getContent(), result.content());
-        assertEquals(task.getCreationDate().toLocalDate().toString(), outputTaskDTO.creationDate());
-        assertEquals(task.getCreationDate().toLocalTime().toString(), outputTaskDTO.creationTime());
-        assertEquals(task.getExpirationDate().toLocalDate().toString(), outputTaskDTO.expirationDate());
-        assertEquals(task.getExpirationDate().toLocalTime().toString(), outputTaskDTO.expirationTime());
-        assertEquals(responseTask.getId(), outputTaskDTO.id());
+        OutputTaskDTO actualReturn = saveTaskUseCase.execute(mockTaskDTO);
+        assertNotNull(actualReturn);
     }
 }
