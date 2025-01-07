7267603ca1b9a866451f0ba5e49014825cf020cc
Joao Oliveira <88111035+jjoaooliveira@users.noreply.github.com>
2025-01-07 00:18:54 -0400

    creating delete task use case test

---
+++ usecase/src/test/java/TaskUseCase/DeleteTaskUseCaseTest.java
@@ -0,0 +1,31 @@
+package TaskUseCase;
+
+import com.webapp.usecase.dataaccess.TaskRepository;
+import com.webapp.usecase.task.DeleteTaskUseCase;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class DeleteTaskUseCaseTest {
+    @Mock
+    TaskRepository mockTaskRepository;
+    @InjectMocks
+    DeleteTaskUseCase deleteTaskUseCase;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void whenExecute_thenCallRepositoryMethodDeleteOneTime() {
+        long aLong = 1L;
+        deleteTaskUseCase.execute(aLong);
+        verify(mockTaskRepository, times(1)).delete(aLong);
+    }
+}
