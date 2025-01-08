cd73ed038abd16074dae7e1aee404953c982ca26
Joao Oliveira <88111035+jjoaooliveira@users.noreply.github.com>
2025-01-07 21:36:21 -0400

    renaming classes

--- usecase/src/main/java/com/webapp/usecase/task/ReadAllTaskUseCase.java
+++ usecase/src/main/java/com/webapp/usecase/task/ReadAllTaskUseCase.java
@@ -5,12 +5,11 @@ import com.webapp.entity.exceptions.EmptyTextException;
 import com.webapp.entity.exceptions.TextLengthOverLimitException;
 import com.webapp.usecase.SimpleReturnUseCase;
 import com.webapp.usecase.dataaccess.TaskRepository;
-import com.webapp.usecase.dto.task.DatabaseTaskDTO;
 
 import java.util.List;
 
 import com.webapp.usecase.dto.task.OutputTaskDTO;
-import com.webapp.usecase.mapper.DatabaseMapper;
+import com.webapp.usecase.dto.task.TaskDTO;
 import com.webapp.usecase.mapper.TaskMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,30 +19,28 @@ public class ReadAllTaskReturnUseCase extends SimpleReturnUseCase<List<OutputTas
 
     private TaskRepository taskRepository;
     private TaskMapper taskMapper;
-    private DatabaseMapper databaseMapper;
 
     @Autowired
-    public ReadAllTaskReturnUseCase(TaskRepository taskRepository, TaskMapper taskMapper, DatabaseMapper databaseMapper) {
+    public ReadAllTaskReturnUseCase(TaskRepository taskRepository, TaskMapper taskMapper) {
         this.taskRepository = taskRepository;
         this.taskMapper = taskMapper;
-        this.databaseMapper = databaseMapper;
     }
 
     @Override
     public List<OutputTaskDTO> execute() {
-        List<DatabaseTaskDTO> dbTasks = taskRepository.findAll();
+        List<TaskDTO> dbTasks = taskRepository.findAll();
 
         List<Task> tasks = dbTasks.stream()
             .map(task -> {
                 try {
-                    return databaseMapper.createEntity(task);
+                    return taskMapper.toTask(task);
                 } catch (TextLengthOverLimitException | EmptyTextException e) {
                     throw new RuntimeException(e);
                 }
             }).toList();
 
         return tasks.stream()
-            .map(taskMapper::createDTO).toList();
+            .map(taskMapper::toOutputDTO).toList();
     }
 }
 
