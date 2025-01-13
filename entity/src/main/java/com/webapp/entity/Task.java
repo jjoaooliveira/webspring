9ffdfbeb6994869fc3e6210c7042b2b5384b73be
Joao Oliveira <88111035+jjoaooliveira@users.noreply.github.com>
2025-01-13 15:08:49 -0400

    removing date properties & adding timedControl

--- entity/src/main/java/com/webapp/entity/Task.java
+++ entity/src/main/java/com/webapp/entity/Task.java
@@ -1,45 +1,26 @@
 package com.webapp.entity;
 
-import java.time.ZonedDateTime;
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-
 public class Task {
-    private final ZoneId zoneId = ZoneId.of("America/Sao_Paulo");
-    private Long id;
     private Title title;
     private Content content;
-    private ZonedDateTime creationDate;
-    private ZonedDateTime expirationDate;
     private Boolean completed;
-    private Boolean expired;
-    private String timeLeft;
+    private TimedControl timedControl;
+    private boolean expired;
 
-    //front-end params task constructor
-    public Task(Title title, Content content, LocalDateTime expirationDate) {
+    public Task(Title title, Content content, TimedControl timedControl) {
         this.title = title;
         this.content = content;
-        this.creationDate = ZonedDateTime.now(zoneId);
-        this.expirationDate = ZonedDateTime.of(expirationDate, zoneId);
+        this.timedControl = timedControl;
         this.completed = false;
         setExpired();
-        setTimeLeft();
     }
 
-    public Task(Long id, Title title, Content content, String creationDate, String expirationDate, boolean completed) {
-        this.id = id;
+    public Task(Title title, Content content, TimedControl timedControl, boolean completed) {
         this.title = title;
         this.content = content;
-        this.creationDate = ZonedDateTime.parse(creationDate);
-        this.expirationDate = ZonedDateTime.parse(expirationDate);
+        this.timedControl = timedControl;
         this.completed = completed;
         setExpired();
-        setTimeLeft();
-    }
-
-    public Long getId() {
-        return this.id;
     }
 
     public String getTitle() {
@@ -58,65 +39,28 @@ public class Task {
         this.content = content;
     }
 
-    public ZonedDateTime getCreationDate() {
-        return creationDate;
+    public String getCreationDate() {
+        return timedControl.getCreationDate();
     }
 
-    public ZonedDateTime getExpirationDate() {
-        return expirationDate;
+    public String getExpirationDate() {
+        return timedControl.getExpirationDate();
     }
 
-    public void setExpirationDate(ZonedDateTime expirationDate) {
-        //TODO trocar o tipo de parametro e implementar verificacao
-        this.expirationDate = expirationDate;
-        setTimeLeft();
-        setExpired();
+    public void setExpiration(String newExpiration, String zone) {
+        timedControl.setExpiration(newExpiration, zone);
     }
 
     public String getTimeLeft() {
-        return timeLeft;
+        return timedControl.getTimeLeft();
     }
-    
-    private void setTimeLeft() {
-        ZonedDateTime now = ZonedDateTime.now(zoneId);
-        Duration duration = Duration.between(now, expirationDate);
-        long days = duration.toDays();
-        long hours = duration.toHours();
-        long minutes = duration.toMinutes();
-        long seconds = duration.toSeconds();
 
-        if (days > 0) {
-            this.timeLeft = hours % 24 > 0 
-                ? (days + 1) + " dia(s)"
-                : days + " dia(s)";
-            return;
-        } 
-        if (hours > 0) {
-            this.timeLeft = minutes % 60  > 0
-                ? (hours + 1) + " hora(s)"
-                : hours + " hora(s)";
-            return;
-        };
-        if(minutes > 0) {
-            this.timeLeft = seconds % 60 > 0 
-                ? (minutes + 1) + " minuto(s)"
-                : minutes + " minuto(s)";
-            return;
-        }
-
-        this.timeLeft = seconds > 0 
-            ? seconds + " segundo(s)"
-            : "expirado"; 
-    }
-    
     public Boolean isExpired() {
         return expired;
     }
-    
+
     private void setExpired() {
-        this.expired = ZonedDateTime
-            .now(zoneId)
-            .isAfter(expirationDate);
+        this.expired = timedControl.isExpired();
     }
 
     public Boolean isCompleted() {
@@ -129,6 +73,6 @@ public class Task {
 
     @Override
     public String toString() {
-        return getContent() + " - " + getTimeLeft();
+        return getTitle() + " - " + getTimeLeft();
     }
 }
