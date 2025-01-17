9791edaf07f93ca023fe7eb342b4bd818f795688
Joao Oliveira <88111035+jjoaooliveira@users.noreply.github.com>
2025-01-16 23:36:10 -0400

    refactoring constructor and removing convert methods

--- entity/src/main/java/com/webapp/entity/TimeControl.java
+++ entity/src/main/java/com/webapp/entity/TimeControl.java
@@ -7,25 +7,13 @@ public class TimeControl {
     protected final ZoneId DEFAULT_ZONE_ID = ZoneId.of("America/Sao_Paulo");
     private ZonedDateTime creation;
 
-    public TimeControl(String creation, String zone) {
-        Objects.requireNonNull(creation);
-        this.creation = convertToZonedDateTime(creation, zone);
+    public TimeControl() {
+        this.creation = ZonedDateTime.now(DEFAULT_ZONE_ID);
     }
 
     public TimeControl(String creation) {
         Objects.requireNonNull(creation);
         this.creation = ZonedDateTime.parse(creation);
-    }
-
-    protected ZonedDateTime convertToZonedDateTime(String inputTimeString, String inputZone) {
-        ZoneId zone = ZoneId.of(inputZone);
-        LocalDateTime localDateTime = LocalDateTime.parse(inputTimeString);
-        ZonedDateTime convertedZonedDateTime = ZonedDateTime.of(localDateTime, zone);
-        return convertUtcToDefault(convertedZonedDateTime);
-    }
-
-    protected ZonedDateTime convertUtcToDefault(ZonedDateTime target) {
-        return target.withZoneSameInstant(DEFAULT_ZONE_ID);
     }
 
     public String getCreation() {
