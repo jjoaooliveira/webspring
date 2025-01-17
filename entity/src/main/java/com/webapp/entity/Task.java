7d9b2cfe466d98a78b89f123420eb193b2611f5f
Joao Oliveira <88111035+jjoaooliveira@users.noreply.github.com>
2025-01-16 22:48:27 -0400

    changing setter methods

--- entity/src/main/java/com/webapp/entity/Task.java
+++ entity/src/main/java/com/webapp/entity/Task.java
@@ -1,5 +1,8 @@
 package com.webapp.entity;
 
+import com.webapp.entity.exceptions.EmptyTextException;
+import com.webapp.entity.exceptions.TextLengthOverLimitException;
+
 public class Task {
     private String id;
     private Title title;
@@ -33,16 +36,16 @@ public class Task {
         return title.getText();
     }
 
-    public void setTitle(Title title) {
-        this.title = title;
+    public void setTitle(String title) {
+        this.title = new Title(title);
     }
 
     public String getContent() {
         return content.getText();
     }
 
-    public void setContent(Content content) {
-        this.content = content;
+    public void setContent(String content) throws TextLengthOverLimitException, EmptyTextException {
+        this.content = new Content(content);
     }
 
     public String getCreationDate() {
