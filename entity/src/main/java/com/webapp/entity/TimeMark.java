b725a3a51c206fed3e162f4e91e1cb388760cd21
Joao Oliveira <88111035+jjoaooliveira@users.noreply.github.com>
2025-01-17 13:28:35 -0400

    renaming class

---
+++ entity/src/main/java/com/webapp/entity/TimeMark.java
@@ -0,0 +1,36 @@
+package com.webapp.entity;
+
+import java.time.*;
+import java.util.Objects;
+
+public class TimeMark {
+    protected final ZoneId DEFAULT_ZONE_ID = ZoneId.of("America/Sao_Paulo");
+    private ZonedDateTime creation;
+
+    public TimeMark() {
+        this.creation = ZonedDateTime.now(DEFAULT_ZONE_ID);
+    }
+
+    public TimeMark(String creation) {
+        Objects.requireNonNull(creation);
+        this.creation = ZonedDateTime.parse(creation);
+    }
+
+    public String getCreation() {
+        return creation.toString();
+    }
+
+    public String getCreationDate() {
+        LocalDate creationLocalDate = creation.toLocalDate();
+        return creationLocalDate.toString();
+    }
+
+    public String getCreationTime() {
+        LocalTime creationLocalTime = creation.toLocalTime();
+        return creationLocalTime.toString();
+    }
+
+    public LocalDateTime getCreationLocalDateTime() {
+        return creation.toLocalDateTime();
+    }
+}
