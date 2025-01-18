e1028a1ce3d21f76a8091ccfd8fd6451cc9f6a75
Joao Oliveira <88111035+jjoaooliveira@users.noreply.github.com>
2025-01-17 22:04:41 -0400

    creating annotation runtime exception

---
+++ usecase/src/main/java/com/webapp/usecase/exception/FailToCreateAnnotationException.java
@@ -0,0 +1,7 @@
+package com.webapp.usecase.exception;
+
+public class FailToCreateAnnotationException extends RuntimeException {
+    public FailToCreateAnnotationException(String message) {
+        super(message);
+    }
+}
