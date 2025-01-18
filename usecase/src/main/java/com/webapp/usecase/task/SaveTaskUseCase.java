a94122453ab28733d3ba7abdcf2de621c8b07c9c
Joao Oliveira <88111035+jjoaooliveira@users.noreply.github.com>
2025-01-17 21:56:02 -0400

    removing unused imports and refactoring execute method

--- usecase/src/main/java/com/webapp/usecase/task/SaveTaskUseCase.java
+++ usecase/src/main/java/com/webapp/usecase/task/SaveTaskUseCase.java
@@ -4,14 +4,13 @@ import com.webapp.entity.Task;
 import com.webapp.entity.exceptions.EmptyTextException;
 import com.webapp.entity.exceptions.TextLengthOverLimitException;
 import com.webapp.usecase.UseCase;
-import com.webapp.usecase.dataaccess.TaskRepository;
-import com.webapp.usecase.dto.task.NewTaskDTO;
+import com.webapp.usecase.data_access.TaskRepository;
+import com.webapp.usecase.dto.task.InputTaskDTO;
 import com.webapp.usecase.dto.task.OutputTaskDTO;
-import com.webapp.usecase.dto.task.TaskDTO;
 import com.webapp.usecase.mapper.TaskMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class SaveTaskUseCase extends UseCase<NewTaskDTO, OutputTaskDTO> {
+public class SaveTaskUseCase extends UseCase<InputTaskDTO, OutputTaskDTO> {
 
     private final TaskRepository taskRepository;
     private final TaskMapper taskMapper;
@@ -23,14 +22,14 @@ public class SaveTaskUseCase extends UseCase<NewTaskDTO, OutputTaskDTO> {
     }
 
     @Override
-    public OutputTaskDTO execute(NewTaskDTO newTaskDTO) {
+    public OutputTaskDTO execute(InputTaskDTO inputTaskDTO) {
         try {
-            Task task = taskMapper.toNewTask(newTaskDTO);
-            TaskDTO taskDTO = taskMapper.toTaskDTO(task);
-            TaskDTO returnedDatabaseTask = taskRepository.save(taskDTO);
-            Task returnedPersistedTask = taskMapper.toTask(returnedDatabaseTask);
-            return taskMapper.toOutputDTO(returnedPersistedTask);
-        } catch (TextLengthOverLimitException | EmptyTextException e) { //TODO implement custom exception
+            Task task = taskMapper.toTask(inputTaskDTO);
+            Task persistedTask = taskRepository.save(task);
+
+            return taskMapper.toOutputDTO(persistedTask);
+        } catch (TextLengthOverLimitException | EmptyTextException e) {
+            //TODO implement custom runtime exception
             throw new RuntimeException(e);
         }
     }
