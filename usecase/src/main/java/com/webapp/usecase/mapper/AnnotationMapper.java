7a6ffd555abc8f995428ec02a3d944b1117fe6b8
Joao Oliveira <88111035+jjoaooliveira@users.noreply.github.com>
2025-01-17 20:06:36 -0400

    creating annotation mapper

--- usecase/src/main/java/com/webapp/usecase/mapper/AnnotationMapper.java
+++ usecase/src/main/java/com/webapp/usecase/mapper/AnnotationMapper.java
@@ -2,57 +2,43 @@ package com.webapp.usecase.mapper;
 
 import com.webapp.entity.Annotation;
 import com.webapp.entity.Content;
+import com.webapp.entity.TimeMark;
 import com.webapp.entity.Title;
 import com.webapp.entity.exceptions.EmptyTextException;
 import com.webapp.entity.exceptions.TextLengthOverLimitException;
-import com.webapp.usecase.dto.annotation.AnnotationDTO;
-import com.webapp.usecase.dto.annotation.NewAnnotationDTO;
+import com.webapp.usecase.dto.annotation.InputAnnotationDTO;
 import com.webapp.usecase.dto.annotation.OutputAnnotationDTO;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnnotationMapper {
     public OutputAnnotationDTO toOutputDTO(Annotation annotation) {
-        String creationDate = annotation.getCreationDate().toLocalDate().toString();
-        String creationTime = annotation.getCreationDate().toLocalTime().toString();
-
         return new OutputAnnotationDTO(
                 annotation.getId(),
                 annotation.getTitle(),
                 annotation.getContent(),
-                creationDate,
-                creationTime
+                annotation.getCreationDate(),
+                annotation.getCreationTime()
         );
     }
 
-    public Annotation toAnnotation(AnnotationDTO annotationDTO) throws TextLengthOverLimitException, EmptyTextException {
-        Title title = new Title(annotationDTO.title());
-        Content content = Content.create(annotationDTO.content());
+    public Annotation toAnnotation(InputAnnotationDTO inputAnnotationDTO) throws TextLengthOverLimitException, EmptyTextException {
+        Title title = new Title(inputAnnotationDTO.title());
+        Content content = new Content(inputAnnotationDTO.content());
+        TimeMark timeMark = new TimeMark();
 
+        if(inputAnnotationDTO.id().isPresent()) {
+            return new Annotation(
+                    inputAnnotationDTO.id().get(),
+                    title,
+                    content,
+                    timeMark
+            );
+        }
         return new Annotation(
-                annotationDTO.id(),
                 title,
                 content,
-                annotationDTO.creationDate()
-        );
-    }
-
-    public AnnotationDTO toAnnotationDTO(Annotation annotation)  {
-        return new AnnotationDTO(
-                annotation.getId(),
-                annotation.getTitle(),
-                annotation.getContent(),
-                annotation.getCreationDate().toString()
-        );
-    }
-
-    public Annotation toAnnotation(NewAnnotationDTO newAnnotation) throws TextLengthOverLimitException, EmptyTextException {
-        Title title = new Title(newAnnotation.title());
-        Content content = Content.create(newAnnotation.content());
-
-        return new Annotation(
-                title,
-                content
+                timeMark
         );
     }
 }
