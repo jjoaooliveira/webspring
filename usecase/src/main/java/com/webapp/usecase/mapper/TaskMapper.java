95c435895421bceb80efe56b9dcf37ee5a56c895
Joao Oliveira <88111035+jjoaooliveira@users.noreply.github.com>
2025-01-17 20:51:14 -0400

    removing toTaskDTO & toNewTask methods

--- usecase/src/main/java/com/webapp/usecase/mapper/TaskMapper.java
+++ usecase/src/main/java/com/webapp/usecase/mapper/TaskMapper.java
@@ -1,69 +1,51 @@
 package com.webapp.usecase.mapper;
 
-import com.webapp.entity.Content;
-import com.webapp.entity.Task;
-import com.webapp.entity.Title;
+import com.webapp.entity.*;
 import com.webapp.entity.exceptions.EmptyTextException;
 import com.webapp.entity.exceptions.TextLengthOverLimitException;
-import com.webapp.usecase.dto.task.NewTaskDTO;
+import com.webapp.usecase.dto.task.InputTaskDTO;
 import com.webapp.usecase.dto.task.OutputTaskDTO;
-import com.webapp.usecase.dto.task.TaskDTO;
 
 import java.time.LocalDateTime;
 
 public class TaskMapper {
     public OutputTaskDTO toOutputDTO(Task task) {
-        String creationDate = task.getCreationDate().toLocalDate().toString();
-        String creationTime = task.getCreationDate().toLocalTime().toString();
-        String expirationDate = task.getExpirationDate().toLocalDate().toString();
-        String expirationTime = task.getExpirationDate().toLocalTime().toString();
         
         return new OutputTaskDTO(
                 task.getId(),
                 task.getTitle(),
                 task.getContent(),
-                creationDate,
-                creationTime,
-                expirationDate,
-                expirationTime,
+                task.getCreationDate(),
+                task.getCreationTime(),
+                task.getExpirationDate(),
+                task.getExpirationTime(),
                 task.getTimeLeft(),
                 task.isCompleted(),
                 task.isExpired()
         );
     }
 
-    public TaskDTO toTaskDTO(Task task) {
-        return new TaskDTO(
-            task.getId(),
-            task.getTitle(),
-            task.getContent(),
-            task.getCreationDate().toString(),
-            task.getExpirationDate().toString(),
-            task.isCompleted()
-        );
-    }
+    public Task toTask(InputTaskDTO inputTaskDTO) throws TextLengthOverLimitException, EmptyTextException {
+        Title title = new Title(inputTaskDTO.title());
+        Content content = new Content(inputTaskDTO.content());
+        TimeMark timeMark = new TimeMark();
+        TimedMark timedMark = new TimedMark(inputTaskDTO.expirationDate(), inputTaskDTO.zone());
 
-    public Task toTask(TaskDTO taskDTO) throws TextLengthOverLimitException, EmptyTextException {
-        Title title = new Title(taskDTO.title());
-        Content content = Content.create(taskDTO.content());
-        LocalDateTime expirationDate = LocalDateTime.parse(taskDTO.expirationDate());
-
+        if(inputTaskDTO.id().isPresent() && inputTaskDTO.completed().isPresent()) {
+            return new Task(
+                    inputTaskDTO.id().get(),
+                    title,
+                    content,
+                    timeMark,
+                    timedMark,
+                    inputTaskDTO.completed().get()
+            );
+        }
         return new Task(
                 title,
                 content,
-                expirationDate
-        );
-    }
-
-    public Task toNewTask(NewTaskDTO newTaskDTO) throws TextLengthOverLimitException, EmptyTextException {
-        Title title = new Title(newTaskDTO.title());
-        Content content = Content.create(newTaskDTO.content());
-        LocalDateTime expirationDate = LocalDateTime.parse(newTaskDTO.expirationDate());
-
-        return new Task(
-                title,
-                content,
-                expirationDate
+                timeMark,
+                timedMark
         );
     }
 
