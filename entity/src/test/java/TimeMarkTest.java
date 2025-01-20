7f32b72c9beea0c18aed92cb71ad758a1f0d2847
Joao Oliveira <88111035+jjoaooliveira@users.noreply.github.com>
2025-01-19 22:36:37 -0400

    removing test, renaming and refactoring

--- entity/src/test/java/TimeMarkTest.java
+++ entity/src/test/java/TimeMarkTest.java
@@ -2,23 +2,19 @@ import com.webapp.entity.TimeMark;
 import org.junit.jupiter.api.Test;
 
 import java.time.DateTimeException;
+import java.time.OffsetDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TimeMarkTest {
     @Test
-    void givenStringCreation_whenGetCreation_thenReturnStringCreation() {
-        String expectCreation = "2024-12-25T15:30-03:00[America/Sao_Paulo]";
+    void givenOffsetDateTime_whenGetCreation_thenReturnStringCreation() {
+        String expectCreation = "2024-12-25T15:30-03:00";
 
-        TimeMark timeMark = new TimeMark("2024-12-25T15:30-03:00[America/Sao_Paulo]");
-        String actualCreation = timeMark.getCreation();
+        TimeMark timeMark = new TimeMark(OffsetDateTime.parse("2024-12-25T15:30-03:00"));
+        var actualCreation = timeMark.getCreation();
 
-        assertEquals(expectCreation, actualCreation);
-    }
-
-    @Test
-    void givenInvalidStringDateTime_whenCreatingTimeMark_thenThrownDateTimeException() {
-        assertThrows(DateTimeException.class, () -> new TimeMark("Lorem"));
+        assertEquals(expectCreation, actualCreation.toString());
     }
 
     @Test
