23e3574d44179d4d2728d01d7678bd4665a1f94b
Joao Oliveira <88111035+jjoaooliveira@users.noreply.github.com>
2025-01-20 17:31:16 -0400

    removing zone parameter

--- usecase/src/main/java/com/webapp/usecase/dto/annotation/InputAnnotationDTO.java
+++ usecase/src/main/java/com/webapp/usecase/dto/annotation/InputAnnotationDTO.java
@@ -1,10 +1,6 @@
 package com.webapp.usecase.dto.annotation;
 
-import java.util.Optional;
-
 public record InputAnnotationDTO(
-        Optional<String> id,
         String title,
-        String content,
-        String zone) {
+        String content) {
 }
