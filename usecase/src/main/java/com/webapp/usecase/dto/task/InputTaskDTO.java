2ba27f02660a2d7b5978c6095262ae255ffba1fb
Joao Oliveira <88111035+jjoaooliveira@users.noreply.github.com>
2025-01-20 17:33:30 -0400

    removing id, zone and completed parameters

--- usecase/src/main/java/com/webapp/usecase/dto/task/InputTaskDTO.java
+++ usecase/src/main/java/com/webapp/usecase/dto/task/InputTaskDTO.java
@@ -1,15 +1,9 @@
 package com.webapp.usecase.dto.task;
 
-import java.util.Optional;
+import java.time.OffsetDateTime;
 
-/**
- * Record para a criação de novos registros
- * */
 public record InputTaskDTO(
-        Optional<String> id,
         String title,
         String content,
-        String expirationDate,
-        String zone,
-        Optional<Boolean> completed
+        OffsetDateTime expirationDate
 ) {}
