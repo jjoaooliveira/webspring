1969a5fb9ccfa004cbcc2d7e0e93d72ba8e1644e
Joao Oliveira <88111035+jjoaooliveira@users.noreply.github.com>
2025-01-19 22:05:49 -0400

    adding new constructor, test methods and changing return type

--- entity/src/main/java/com/webapp/entity/TimedMark.java
+++ entity/src/main/java/com/webapp/entity/TimedMark.java
@@ -4,37 +4,33 @@ import java.time.*;
 import java.util.Objects;
 
 public class TimedMark {
-    protected final ZoneId TARGET_ZONE_ID = ZoneId.of("America/Sao_Paulo");
+    protected final ZoneId DEFAULT_ZONE_ID = ZoneId.of("America/Sao_Paulo");
     private ZonedDateTime expiration;
 
-    public TimedMark(String expiration, String zone) {
-        Objects.requireNonNull(expiration);
-        this.expiration = convertToZonedDateTime(expiration, zone);
+    public TimedMark(ZonedDateTime zonedDateTime) {
+        Objects.requireNonNull(zonedDateTime);
+        this.expiration = convertToDefaultUTC(zonedDateTime);
     }
 
-    protected ZonedDateTime convertToZonedDateTime(String inputTimeString, String inputZone) {
-        ZoneId zone = ZoneId.of(inputZone);
-        LocalDateTime localDateTime = LocalDateTime.parse(inputTimeString);
-        ZonedDateTime convertedZonedDateTime = ZonedDateTime.of(localDateTime, zone);
-        return convertUtcToDefault(convertedZonedDateTime);
+    public TimedMark(OffsetDateTime offsetDateTime) {
+        Objects.requireNonNull(offsetDateTime);
+        this.expiration = offsetDateTime.toZonedDateTime();
     }
 
-    private ZonedDateTime convertUtcToDefault(ZonedDateTime target) {
-        return target.withZoneSameInstant(TARGET_ZONE_ID);
+    private ZonedDateTime convertToDefaultUTC(ZonedDateTime zonedDateTime) {
+        return zonedDateTime.withZoneSameInstant(DEFAULT_ZONE_ID);
     }
 
-    public String getExpiration() {
-        return expiration.toString();
+    public OffsetDateTime getExpiration() {
+        return expiration.toOffsetDateTime();
     }
 
-    public String getExpirationDate() {
-        LocalDate expirationLocalDate = expiration.toLocalDate();
-        return expirationLocalDate.toString();
+    public LocalDate getExpirationDate() {
+        return expiration.toLocalDate();
     }
 
-    public String getExpirationTime() {
-        LocalTime expirationLocalTime = expiration.toLocalTime();
-        return expirationLocalTime.toString();
+    public LocalTime getExpirationTime() {
+        return expiration.toLocalTime();
     }
 
     public LocalDateTime getExpirationLocalDateTime() {
@@ -42,12 +38,17 @@ public class TimedMark {
     }
 
     public boolean isExpired() {
-        ZonedDateTime now = ZonedDateTime.now(TARGET_ZONE_ID);
+        ZonedDateTime now = ZonedDateTime.now(DEFAULT_ZONE_ID);
+        return now.isAfter(expiration);
+    }
+
+    public boolean isExpired(Clock clock) {
+        ZonedDateTime now = ZonedDateTime.now(clock);
         return now.isAfter(expiration);
     }
 
     public String getTimeLeft() {
-        ZonedDateTime now = ZonedDateTime.now(TARGET_ZONE_ID);
+        ZonedDateTime now = ZonedDateTime.now(DEFAULT_ZONE_ID);
         Duration duration = Duration.between(now, expiration);
         long days = duration.toDays();
         long hours = duration.toHours();
@@ -67,6 +68,29 @@ public class TimedMark {
         }
 
         return seconds > 0 ? seconds + "s" : "0s";
+    }
+
+    public String getTimeLeft(Clock clock) {
+        ZonedDateTime now = ZonedDateTime.now(clock);
+        Duration duration = Duration.between(now, expiration);
+        long days = duration.toDays();
+        long hours = duration.toHours();
+        long minutes = duration.toMinutes();
+        long seconds = duration.toSeconds();
+
+        if (days > 0) {
+            return days + " d";
+        }
+
+        if (hours > 0) {
+            return (hours % 24) + " h";
+        }
+
+        if(minutes > 0) {
+            return (minutes % 60) + " min";
+        }
+
+        return seconds > 0 ? seconds + " s" : "0s";
     }
 
 }
