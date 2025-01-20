f0fd16ab54992f8e741f720f024978cf4d862095
Joao Oliveira <88111035+jjoaooliveira@users.noreply.github.com>
2025-01-20 17:04:43 -0400

    adding notation and removing if statement

--- usecase/src/main/java/com/webapp/usecase/mapper/TaskMapper.java
+++ usecase/src/main/java/com/webapp/usecase/mapper/TaskMapper.java
@@ -5,18 +5,20 @@ import com.webapp.entity.exceptions.EmptyTextException;
 import com.webapp.entity.exceptions.TextLengthOverLimitException;
 import com.webapp.usecase.dto.task.InputTaskDTO;
 import com.webapp.usecase.dto.task.OutputTaskDTO;
+import org.springframework.stereotype.Component;
 
+import java.time.ZonedDateTime;
+
+@Component
 public class TaskMapper {
     public OutputTaskDTO toOutputDTO(Task task) {
-        
+
         return new OutputTaskDTO(
                 task.getId(),
                 task.getTitle(),
                 task.getContent(),
-                task.getCreationDate(),
-                task.getCreationTime(),
-                task.getExpirationDate(),
-                task.getExpirationTime(),
+                task.getCreation(),
+                task.getExpiration(),
                 task.getTimeLeft(),
                 task.isCompleted(),
                 task.isExpired()
@@ -27,18 +29,11 @@ public class TaskMapper {
         Title title = new Title(inputTaskDTO.title());
         Content content = new Content(inputTaskDTO.content());
         TimeMark timeMark = new TimeMark();
-        TimedMark timedMark = new TimedMark(inputTaskDTO.expirationDate(), inputTaskDTO.zone());
 
-        if(inputTaskDTO.id().isPresent() && inputTaskDTO.completed().isPresent()) {
-            return new Task(
-                    inputTaskDTO.id().get(),
-                    title,
-                    content,
-                    timeMark,
-                    timedMark,
-                    inputTaskDTO.completed().get()
-            );
-        }
+        ZonedDateTime zonedDateTime = inputTaskDTO.expirationDate().toZonedDateTime();
+
+        TimedMark timedMark = new TimedMark(zonedDateTime);
+
         return new Task(
                 title,
                 content,
