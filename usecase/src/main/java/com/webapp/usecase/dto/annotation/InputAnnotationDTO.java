7bb418caa1def583a661b5ed44750afee2bea81d
Joao Oliveira <88111035+jjoaooliveira@users.noreply.github.com>
2025-01-31 16:43:18 -0400

    deleting updateDTO and adding new fields to inputDTO

--- usecase/src/main/java/com/webapp/usecase/dto/annotation/InputAnnotationDTO.java
+++ usecase/src/main/java/com/webapp/usecase/dto/annotation/InputAnnotationDTO.java
@@ -1,6 +1,10 @@
 package com.webapp.usecase.dto.annotation;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public record InputAnnotationDTO(
+        Optional<UUID> id,
         String title,
         String content) {
 }
--- usecase/src/main/java/com/webapp/usecase/dto/task/InputTaskDTO.java
+++ usecase/src/main/java/com/webapp/usecase/dto/task/InputTaskDTO.java
@@ -1,9 +1,13 @@
 package com.webapp.usecase.dto.task;
 
 import java.time.OffsetDateTime;
+import java.util.Optional;
+import java.util.UUID;
 
 public record InputTaskDTO(
+        Optional<UUID> id,
         String title,
         String content,
-        OffsetDateTime expirationDate
+        OffsetDateTime expirationDate,
+        Boolean completed
 ) {}
