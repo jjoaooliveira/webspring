b599e80726464509dd5b12570cff7e819f03cc44
Joao Oliveira <88111035+jjoaooliveira@users.noreply.github.com>
2025-02-05 16:06:46 -0400

    creating read annotation by title use case class

---
+++ usecase/src/main/java/com/webapp/usecase/annotation/ReadAnnotationByTitleUseCase.java
@@ -0,0 +1,27 @@
+package com.webapp.usecase.annotation;
+
+import com.webapp.entity.Annotation;
+import com.webapp.usecase.UseCase;
+import com.webapp.usecase.data_access.AnnotationDataAccess;
+import com.webapp.usecase.dto.annotation.OutputAnnotationDTO;
+import com.webapp.usecase.mapper.AnnotationMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class ReadAnnotationByTitleUseCase extends UseCase<String, List<OutputAnnotationDTO>> {
+    private final AnnotationDataAccess annotationDataAccess;
+    private final AnnotationMapper annotationMapper;
+
+    @Autowired
+    public ReadAnnotationByTitleUseCase(AnnotationDataAccess annotationDataAccess, AnnotationMapper annotationMapper) {
+        this.annotationDataAccess = annotationDataAccess;
+        this.annotationMapper = annotationMapper;
+    }
+
+    @Override
+    public List<OutputAnnotationDTO> execute(String title) {
+        List<Annotation> annotationList = annotationDataAccess.findByTitle(title);
+        return annotationList.stream().map(annotationMapper::toOutputDTO).toList();
+    }
+}
