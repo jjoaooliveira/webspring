2d5c88a558ee902bfd2aba29c5c4dfc8db79fb1b
Joao Oliveira <88111035+jjoaooliveira@users.noreply.github.com>
2025-02-08 19:27:44 -0400

    creating annotation and task presenters

---
+++ adapter/src/main/java/com/webapp/presenter/AnnotationPresenter.java
@@ -0,0 +1,32 @@
+package com.webapp.presenter;
+
+import com.webapp.controller.web.AnnotationController;
+import com.webapp.usecase.dto.annotation.OutputAnnotationDTO;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+
+import java.util.Collection;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+public class AnnotationPresenter {
+    public EntityModel<OutputAnnotationDTO> toEntityModel(OutputAnnotationDTO outputAnnotationDTO) {
+        return EntityModel.of(
+                outputAnnotationDTO,
+                linkTo(methodOn(AnnotationController.class)
+                        .getAnnotationById(outputAnnotationDTO.id()))
+                        .withSelfRel(),
+                linkTo(methodOn(AnnotationController.class)
+                        .getAllAnnotation())
+                        .withRel("annotations"));
+    }
+
+    public CollectionModel<EntityModel<OutputAnnotationDTO>> toCollectionModel(Collection<EntityModel<OutputAnnotationDTO>> collection) {
+        return CollectionModel.of(
+                collection,
+                linkTo(methodOn(AnnotationController.class)
+                        .getAllAnnotation())
+                        .withSelfRel());
+    }
+}
