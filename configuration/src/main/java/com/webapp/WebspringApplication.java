94887919b9d0254060c540c311a30881c3ebf102
Joao Oliveira <88111035+jjoaooliveira@users.noreply.github.com>
2025-02-20 00:22:22 -0400

    creating application main

---
+++ configuration/src/main/java/com/webapp/WebspringApplication.java
@@ -0,0 +1,12 @@
+package com.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebspringApplication.class, args);
+	}
+}
