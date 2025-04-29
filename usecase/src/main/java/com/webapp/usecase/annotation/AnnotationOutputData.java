c2b9d766b2161c1a7919556aa52a20a4993f4306
Joao Oliveira <88111035+jjoaooliveira@users.noreply.github.com>
2025-04-29 14:21:10 -0400

    refactor(taskOutputData/annotationOutputData): change record type to class

--- usecase/src/main/java/com/webapp/usecase/annotation/AnnotationOutputData.java
+++ usecase/src/main/java/com/webapp/usecase/annotation/AnnotationOutputData.java
@@ -1,12 +1,45 @@
 package com.webapp.usecase.annotation;
 
-import java.time.Instant;
 import java.util.UUID;
 
-public record AnnotationOutputData(
-        UUID id,
-        String title,
-        String content,
-        Instant creationDate
-) {
+public class AnnotationOutputData {
+    private final UUID id;
+    private String title;
+    private String content;
+    private String creationDate;
+
+    public AnnotationOutputData(UUID id, String title, String content, String creationDate) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.creationDate = creationDate;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
 }
--- usecase/src/main/java/com/webapp/usecase/task/TaskOutputData.java
+++ usecase/src/main/java/com/webapp/usecase/task/TaskOutputData.java
@@ -1,13 +1,65 @@
 package com.webapp.usecase.task;
 
-import java.time.Instant;
 import java.util.UUID;
 
-public record TaskOutputData(
-        UUID uuid,
-        String title,
-        String content,
-        Instant creation,
-        Instant expiration,
-        boolean completed
-) {}
+public class TaskOutputData {
+    private final UUID uuid;
+    private String title;
+    private String content;
+    private String creation;
+    private String expiration;
+    private boolean completed;
+
+    public TaskOutputData(UUID uuid, String title, String content, String creation, String expiration, boolean completed) {
+        this.uuid = uuid;
+        this.title = title;
+        this.content = content;
+        this.creation = creation;
+        this.expiration = expiration;
+        this.completed = completed;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreation() {
+        return creation;
+    }
+
+    public void setCreation(String creation) {
+        this.creation = creation;
+    }
+
+    public String getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(String expiration) {
+        this.expiration = expiration;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
